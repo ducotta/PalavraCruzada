679baab1cbea1c11a9225add45a6a6a743d224e9
Eduardo L Cotta <ducotta@gmail.com>
2013-10-29 23:32:02 -0200

    corrigindo alguns bugs e reformulando o construtor

--- palavraCruzada/PalavraCruzada.java
+++ palavraCruzada/PalavraCruzada.java
@@ -2,18 +2,15 @@ package palavraCruzada;
 
 public class PalavraCruzada {
 	
-	int matriz[][];
-	int tamanhoI = 5;
-	int tamanhoJ = 8;
-	int contador = 1;
-	public PalavraCruzada(int[][] matriz, int tamanhoI, int tamanhoJ) {
-		super();
-		this.matriz = matriz;
-		this.tamanhoI = tamanhoI;
-		this.tamanhoJ = tamanhoJ;
-	}
+	int tamanhoI;
+	int tamanhoJ;
+	int [][] matriz;
 	
-	//matriz[][] = matriz[tamanho][tamanho];
+	public PalavraCruzada( int tamanhoi, int tamanhoj) {
+		tamanhoI = tamanhoi;
+		tamanhoJ = tamanhoj;
+		matriz = new int [tamanhoI][tamanhoJ];
+	}
 	
 	public int[][] getMatriz() {
 		return matriz;
@@ -41,21 +38,19 @@ public class PalavraCruzada {
 		this.tamanhoJ = tamanhoJ;
 	}
 
-
-	public int getContador() {
-		return contador;
-	}
-
-
-	public void setContador(int contador) {
-		this.contador = contador;
-	}
-
-	public int transformaMatriz (int i, int j){
-		for (i=0;i<tamanhoI;i++) {
+	public int [][] transformaMatriz (){
+		int i = 0;
+		int j = 0;
+		int contador = 1;
+		
+		
+		//System.out.print(matriz[i][j] + "\n");
+		
+		for (i=0;i<=tamanhoI;i++) {
 			for(j=0;j<tamanhoJ;j++){
-				if(matriz[i][j] == -1) continue;
-				else{
+				if(matriz[i][j] == -1){
+					matriz[i][j] = -1;
+				}else{
 					if(((i == 0 && j == 0)) && ((matriz[i+1][j] == 0 || matriz[i][j+1] == 0))){
 						matriz[i][j] = contador;
 						contador = contador + 1;
@@ -80,7 +75,7 @@ public class PalavraCruzada {
 						matriz[i][j] = contador;
 						contador = contador + 1;
 					}
-					if((0 < i && i < tamanhoI-1 && j == tamanhoJ-1) && (matriz[i-1][j] == -1 && matriz[1+1][j] == 0)){
+					if((0 < i && i < tamanhoI-1 && j == tamanhoJ-1) && (matriz[i-1][j] == -1 && matriz[i+1][j] == 0)){
 						matriz[i][j] = contador;
 						contador = contador + 1;
 					}
@@ -89,10 +84,12 @@ public class PalavraCruzada {
 						contador = contador + 1;
 					}
 				}
+				System.out.print(matriz[i][j] + "\t");
 			}
-		
+			System.out.print("\n");
 		}
-		return matriz [tamanhoI][tamanhoJ];
+		
+		return matriz;
 	}
 	
 	
